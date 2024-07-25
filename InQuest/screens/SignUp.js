64e80e62c2d93c42dd8608fd6238d1ea0b6b9936
Tharaa40 <95153704+Tharaa40@users.noreply.github.com>
2024-07-26 00:23:59 +0800

    RESTART
    - created all the screens except calendar
    - can create and save task
    Card in the homepage:
    - progress circle updates based on subtasks completed
    - checking off will remove card from homepage, not from the database and from the Details screen
    
    - deleting task from details page deletes it completely from the database

---
+++ InQuest/screens/SignUp.js
@@ -0,0 +1,56 @@
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+export default function SignUps(){
+    const navigation = useNavigation();
+    return(
+        <View style={styles.container}>
+            <Text style = {styles.title} > InQuest </Text>  
+            <TouchableOpacity style = {styles.buttonEmail} onPress={() => navigation.navigate('Email Sign Up')}>
+                <Text style={styles.buttonText}>Sign Up with Email</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style = {styles.buttonGoogle}>
+                <Text style={styles.buttonText}>Sign Up with Google</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1, 
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: '#040D12'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 150,
+        marginTop: 150,
+        color: '#93B1A6',
+    },
+    buttonEmail: {
+        backgroundColor: '#5C8374', // Change this to your desired button color
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 20, // Rounded corners
+        marginBottom: 50
+    },
+    buttonGoogle: {
+        backgroundColor: '#183D3D', // Change this to your desired button color
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 20, // Rounded corners
+    },
+    buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    },
+
+});
